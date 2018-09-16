10c8cb90f185f70779afa75dedf4a23051b52dfa
uribea <aauribe@miners.utep.edu>
2018-09-15 23:03:47 -0600

    shell.py has near perfect redirection.

--- Shell/shell.py
+++ Shell/shell.py
@@ -1,14 +1,13 @@
 #! /usr/bin/env python3
 
-import os, sys, time, re
+import os, sys, re
 
 pid = os.getpid()
 #text = ''
-dire = re.split(":", os.environ['PATH'])
-dire = dire[0]
-text = input(dire + "> ")
-while text!="q":
-
+text = input("$ ")
+while text!="exit":
+    args = str.split(text)
+    print(args)
     os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
 
     rc = os.fork()
@@ -22,23 +21,42 @@ while text!="q":
                      (os.getpid(), pid)).encode())
 
         #args = ["wc", "shell.py"]
-        args = re.split(" ", text, 1)
         print(args)
-        if args[0]=='>':
+        if '>' in args:
             os.close(1)  # redirect child's stdout
-            sys.stdout = open(args[1], "w")
+            i = args.index('>')
+            # print(i)
+            del args[i]
+            # print(args)
+            #os.write(2, ("%d\n" % len(args)).encode())
+            #os.write(2, ("%s \n" % args[i]).encode())
+            sys.stdout = open(args[i], "w")
+            args = args[:i]
             fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
             os.set_inheritable(fd, True)
             os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
-#            args = re.split(" ", input("/>"),1)
+            #            args = re.split(" ", input("/>"),1)
 
-        elif args[0]=='<':
+        elif '<' in args:
+            print("why dough")
             os.close(0)  # redirect child's stdin
-            sys.stdin = open(args[1], "r")
+            i = args.index('<')
+            del args[i]
+            sys.stdin = open(args[i], "r")
+            args = args[:i]
             fd = sys.stdin.fileno()  # os.open("p4-output.txt", os.O_CREAT)
             os.set_inheritable(fd, True)
-            args = sys.stdin.read().split()
+            #args = sys.stdin.read().split()
+            os.write(2, ("Child: opened fd=%d for reading\n" % fd).encode())
+        if False:
+            args = ["wc", "shell.py"]
+            print(args)
+            os.close(1)  # redirect child's stdout
+            sys.stdout = open("p4-output.txt", "w")
+            fd = sys.stdout.fileno()  # os.open("p4-output.txt", os.O_CREAT)
+            os.set_inheritable(fd, True)
             os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
+
 
         for dir in re.split(":", os.environ['PATH']): # try each directory in the path
             program = "%s/%s" % (dir, args[0])
@@ -57,4 +75,4 @@ while text!="q":
         childPidCode = os.wait()
         os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
                      childPidCode).encode())
-        text = input(dire[0] + "> ")
+        text = input("$ ")
